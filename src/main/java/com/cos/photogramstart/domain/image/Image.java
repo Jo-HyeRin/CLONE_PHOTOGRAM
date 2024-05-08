7da10d6aec957167a464bef5eddcae77ac8d5400
sayhyelin12@naver.com <sayhyelin12@naver.com>
2024-05-08 17:50:38 +0900

    show home story list

--- src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -11,6 +11,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 
 import com.cos.photogramstart.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,6 +32,7 @@ public class Image {
 	private String caption; // 내용
 	private String postImageUrl; // 사진을 전송받아서 그 사진을 서버 폴더에 저장 - DB에는 그 저장 경로를 insert
 	
+	@JsonIgnoreProperties({"images"}) // 이미지의 user 가 가지고 있는 images는 필요없으니 무시하도록 설정
 	@JoinColumn(name="userId") // user 객체를 저장할 수 없음. 외래키가 저장됨. 외래키의 컬럼명을 설정
 	@ManyToOne
 	private User user; // 업로드 한 사람. 한 명은 여러 개의 이미지를 등록할 수 있다. (user 1 : image N)
--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -3,6 +3,7 @@ package com.cos.photogramstart.service;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -26,6 +27,12 @@ public class ImageService {
 	// yml 파일에 file: path: 로 만들어 두고 해당 값을 가져와서 사용하는 것이 실수도 줄이고 효율적이다.
 	@Value("${file.path}")
 	private String uploadFolder; 
+	
+	@Transactional(readOnly=true) // readOnly=true : 영속성 컨텍스트 변경 감지를 해서 더티체킹, flush(반영)을 안 하게 함.
+	public List<Image> 이미지스토리(int principalId){
+		List<Image> images = imageRepository.mStory(principalId);
+		return images;
+	}
 	
 	@Transactional // 
 	public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
